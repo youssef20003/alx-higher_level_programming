dad510de4ba1c1e1c602ee21e9566a900e6240c8
youssef20003 <ym05914@gmail.com>
2023-09-08 03:26:21 +0300

    a

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-val = add_0.add(a, b)
-print("{:s} + {:s} = {:s}".format(a, b, va))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
