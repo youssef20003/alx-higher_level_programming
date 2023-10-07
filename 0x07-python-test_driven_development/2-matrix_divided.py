9c88aa0f8066de9149a6b5ac659368abddd7bdc6
youssef20003 <ym05914@gmail.com>
2023-10-07 04:24:28 +0300

    a

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,49 +1,59 @@
 #!/usr/bin/python3
-"""Module for matrix_divided method"""
+"""
+This is the divide module.
+has a function to divide matrix
+Divide all elements of a matrix
+"""
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix.
-
-    Args:
-        matrix: The matrix whoses elements are to be divided by div.
-        div: The dividing number.
-
-    Raises:
-        TypeError: if matrix is not a list of lists of int or float.
-        TypeError: if each row of matrix is not of same size.
-        TypeError: if div is neither an int nor float
-        ZeroDivisionError: if div is zero
-
-    Returns:
-        a new matrix with elements rounded to 2 decimal places.
+    """matrix must be a list of lists of integers/floats
+    Returns a new matrix
     """
+    newmatrix = []
+    length = 0
 
-    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
-        raise TypeError("matrix must be a matrix (list of lists) " +
-                        "of integers/floats")
-
+    # Divides all elements of a matrix
+    if isinstance(div, int) is False and isinstance(div, float) is False:
+        raise TypeError('div must be a number')
+    # Matrix must be a list of integers or floats, TypeError
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    if not isinstance(matrix[0], list):
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    if not isinstance(matrix, list):
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    # matix has to exist, can't be less or equal to 0
+    if len(matrix[0]) <= 0:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    # 1.let's create new matrix with newrow
     for row in matrix:
-        if len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) " +
-                            "of integers/floats")
-        for x in row:
-            if type(x) is not int and type(x) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) " +
-                                "of integers/floats")
-
-    len_rows = []
-    for row in matrix:
-        len_rows.append(len(row))
-    if not all(elem == len_rows[0] for elem in len_rows):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
-
-    return new_matrix
+        newrow = []
+        # matrix must be a list
+        if type(row) is not list:
+            raise TypeError('matrix must be a matrix (list of lists) '
+                            'of integers/floats')
+        # 2. row is empty
+        if length is 0:
+            length = len(row)
+        # Each row must be the same size, TypeError
+        elif len(row) is not length:
+            raise TypeError('Each row of the matrix must have the same size')
+        # 3. each item has to be an integer or float
+        for item in row:
+            if type(item) is not int and type(item) is not float:
+                raise TypeError('matrix must be a matrix (list of lists) '
+                                'of integers/floats')
+            # 4. add content to the row
+            # elements will be divided by div and rounded with 2 decimal
+            # round() function returns a floating point number that is
+            # a rounded version of the specified number
+            # with the specified number of decimals
+            newrow.append(round(item / div, 2))
+        # add content to the matrix
+        newmatrix.append(newrow)
+    return newmatrix
